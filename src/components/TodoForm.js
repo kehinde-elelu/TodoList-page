b6603018feea463b3263058fbffc75cce68ee9d9
Kehinde <abdulsalamelelu@gmail.com>
2020-11-12 15:40:38 +0100

    second commit

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -34,13 +34,6 @@ const TodoForm = ({setState, state, todos, setTodos}) => {
             >
             <i className="fas fa-plus-square"></i>
             </button>
-            <div className="select">
-                <select name="todos" className="filter-todo">
-                <option value="all">All</option>
-                <option value="completed">Completed</option>
-                <option value="uncompleted">Uncompleted</option>
-                </select>
-            </div>
         </form>
             
     )
